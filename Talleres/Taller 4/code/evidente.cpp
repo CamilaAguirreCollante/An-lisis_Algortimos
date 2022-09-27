f38b05adbd1b06d419cf7b5a7d571d17fbda0f17
Camila Collante <camilasanchezcollante31@gmail.com>
2022-09-27 12:47:42 -0500

    correction

--- Talleres/Taller 4/code/evidente.cpp
+++ Talleres/Taller 4/code/evidente.cpp
@@ -4,56 +4,46 @@
 
 using namespace std;
 
-vector<int> longestSequence(vector<vector<int>> const &matriz, int i, int j) {
-  vector<int> L;
+int longestSequence(vector<vector<int>> const &matriz, int i, int j) {
+  int recursion = 0;
   int n = matriz.size();
 
   if ((matriz[i][j] - matriz[i - 1][j] == 1) && (i > 0)) {
-    L = longestSequence(matriz, i - 1, j);
+    recursion = longestSequence(matriz, i - 1, j);
   }
   if ((matriz[i][j] - matriz[i][j + 1] == 1) && (j + 1 < n)) {
-    L = longestSequence(matriz, i, j + 1);
+    recursion = longestSequence(matriz, i, j + 1);
   }
   if ((matriz[i][j] - matriz[i + 1][j] == 1) && (i + 1 < n)) {
-    L = longestSequence(matriz, i + 1, j);
+    recursion = longestSequence(matriz, i + 1, j);
   }
   if ((matriz[i][j] - matriz[i][j - 1] == 1) && (j > 0)) {
-    L = longestSequence(matriz, i, j - 1);
+    recursion = longestSequence(matriz, i, j - 1);
   }
-  L.push_back(matriz[i][j]);
-  return L;
+  return recursion+1;
 }
 
-vector<int> longestSequence(vector<vector<int>> const &matriz) {
-  vector<int> seq;
+int longestSequence(vector<vector<int>> const &matriz) {
+  int actual = 0;
   for (int i = 0; i < matriz.size(); i++) {
     for (int j = 0; j < matriz.size(); j++) {
-      vector<int> sequence = longestSequence(matriz, i, j);
-      if (sequence.size() > seq.size()) {
-        seq = sequence;
+      int len = longestSequence(matriz, i, j);
+      if (len > actual) {
+        actual = len;
       }
     }
   }
-  return seq;
+  return actual;
 }
 
-void printSequence(vector<int> const &seq) {
-  cout << "[";
-  int n = seq.size();
-  for (int i = 0; i < n; i++) {
-    cout << seq[i];
-    if (i < n - 1) {
-      cout << ", ";
-    }
-  }
-  cout << "]";
-}
 
 int main() {
   vector<vector<int>> matriz = {
-      {10, 16, 15, 12}, {9, 8, 7, 13}, {2, 5, 6, 14}, {3, 4, 1, 11}};
-  vector<int> L = longestSequence(matriz);
-  printSequence(L);
+      {10, 16, 15, 12}, 
+      {9, 8, 7, 13}, 
+      {2, 5, 6, 14}, 
+      {3, 4, 1, 11}};
+  cout << longestSequence(matriz);
 
   return 0;
 }
